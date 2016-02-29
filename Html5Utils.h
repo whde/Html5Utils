62ff41cccecfe16960f4462d5e916549ddf09e51
whde <whde@Whde-MacBook-Pro.local>
2016-02-29 18:08:24 +0800

    add

---
+++ Html5Utils.h
@@ -0,0 +1,19 @@
+//
+//  Html5Utils.h
+//  Html5Utils
+//
+//  Created by whde on 16/1/26.
+//  Copyright © 2016年 Whde. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIWebView+JavaScript.h"
+//! Project version number for Html5Utils.
+FOUNDATION_EXPORT double Html5UtilsVersionNumber;
+
+//! Project version string for Html5Utils.
+FOUNDATION_EXPORT const unsigned char Html5UtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Html5Utils/PublicHeader.h>
+
+
